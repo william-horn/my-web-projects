ed61dead59e71269190fa8f03cbddea18f269e65
William Horn <williamjosephhorn@gmail.com>
2022-03-13 13:31:00 -0400

    successful line drawing experiment

--- developing/arcade-game/resources/workspace/js/main.js
+++ developing/arcade-game/resources/workspace/js/main.js
@@ -36,11 +36,37 @@ https://github.com/william-horn/my-coding-conventions/blob/main/document-convent
 | DOCUMENT TODO |
 ==================================================================================================================================
 
--   do this
--   do that
+-
 
 ==================================================================================================================================
 */
 
+const $gameGraphicsDisplay = document.querySelector("#main-game-window canvas");
+const gameGraphics = $gameGraphicsDisplay.getContext("2d");
 
+function drawLine(fromX, fromY, toX, toY) {
+    gameGraphics.beginPath();
+    gameGraphics.moveTo(fromX, fromY);
+    gameGraphics.lineTo(toX, toY);
+    gameGraphics.closePath();
+    gameGraphics.stroke();
+}
+
+function rad(deg) {
+    return (deg*Math.PI)/180;
+}
+
+let radius = 100;
+let counter = 0;
+let parts = 200;
+let routine = setInterval(() => {
+    let theta = rad((1 - counter/parts)*90);
+    let x = Math.cos(theta)*radius;
+    let y = Math.sin(theta)*radius;
+    drawLine(0, 0, x, y);
+    counter++;
+    if (counter > parts) {
+        clearInterval(routine);
+    }
+}, 100)
 
