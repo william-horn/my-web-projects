d94aa9ecb0c51739ae0916412c6c3a613e6d6445
William Horn <williamjosephhorn@gmail.com>
2022-03-20 13:26:10 -0400

    initial

--- developing/test-environment/resources/workspace/js/main.js
+++ developing/test-environment/resources/workspace/js/main.js
@@ -45,6 +45,7 @@ Coming soon
 /* ---------------- */
 import jsinit from "../../../../../tools/api/general/js/js-init-1.0.0.js";
 import datastore from "../../../../../tools/api/general/js/datastore-1.0.0.js";
+import EventHandler from "../../../../../tools/api/general/js/event-handler.1.0.0.js";
 
 /* ------------------------- */
 /* Global Element References */
@@ -53,17 +54,7 @@ import datastore from "../../../../../tools/api/general/js/datastore-1.0.0.js";
 /* ----------------------- */
 /* Internal Program States */
 /* ----------------------- */
-const datakeys = datastore.datakeys
-datakeys.saveName = "my-local-data1";
 
-console.log("cache before: ", datastore.cache[datakeys.saveName]);
-const data = datastore.get(datakeys.saveName, {});
-console.log("saved data: ", data);
-console.log("cache after: ", datastore.cache[datakeys.saveName]);
-datastore.update(datakeys.saveName, oldData => {
-    oldData.name = "hey bill";
-    return oldData;
-});
 
 /* ---------------------- */
 /* General Util Functions */
--- tools/api/general/js/event-handler.1.0.0.js
+++ tools/api/general/js/event-handler.1.0.0.js
@@ -47,152 +47,57 @@ import DynamicState from "./dystates-1.0.0.js";
 
 // jQuery support for events
 const isJQ = window.jQuery;
-const eventConnectorName = isJQ? "on" : "addEventListener";
-const eventDisconnectorName = isJQ? "off" : "removeEventListener";
+const eventConnectorName = isJQ ? "on" : "addEventListener";
+const eventDisconnectorName = isJQ ? "off" : "removeEventListener";
 
 const eventHandlerStates = {
     "listening": "listening",
     "paused": "paused"
 }
 
-// bypass jQuery wrapper
-function getRawObject(object) {
-    return isJQ? object.get(0): object;
-}
+// Listener - object
+// EventHandler - object
+
+/* api:
+EventHandler.add("click", "name", object, function)
+EventHandler.add("click", object, function)
+
+EventHandler.remove/pause/resume("click")
+EventHandler.remove/pause/resume("click", "name")
+EventHandler.remove/pause/resume("click", object)
+EventHandler.remove/pause/resume("click", "name", object)
+EventHandler.remove/pause/resume(ListenerObject)
+*/
 
 class Listener extends DynamicState {
-    constructor(eventName, object, func, funcHandler) {
-        super(eventHandlerStates)
+    constructor() {
+        super(eventHandlerStates);
         this.setState("listening");
 
-        this.name = eventName;
-        this.obj = object;
-        this.func = func;
-        this.handler = funcHandler;
-    }
-
-    disconnect() {
-        this.obj[eventDisconnectorName](this.name, this.handler);
-    }
+        this.alias = ""; // arbitrary, optional event name alias
+        this.objRef = ""; // actual reference to the object
+        this.callback = ""; // callback function given by the developer
+        this.handler = "" // handler function that wraps the callback function
+    }  
 }
 
-export default class EventHandler extends Listener {
+class EventHandler extends Listener {
     constructor() {
         super();
-        delete this.disconnect; // don't inherit
-        this.connections = new Map();
-    }
 
-    pauseAll() {
-        this.setState("paused");
-    }
-
-    resumeAll() {
-        this.setState("listening");
-    }
-
-    setAllStates(eventName, object, newState) {
-        if (object) {
-            // args are: eventName=String, object=HTML Element, newState=String
-            const eventData = this.connections.get(getRawObject(object));
-            const selectAll = eventName === "all";
-
-            if (!eventData) return;
-
-            eventData.forEach(listener => {
-                if (listener.name === eventName || selectAll) {
-                    listener.setState(newState);
-                }
-            })
-        } else {
-            // args are: eventName=Listener, object=undefined, newState=String
-            const listener = eventName;
-            listener.setState(newState);
-        }
-    }
-
-    pause(eventName, object) {
-        this.setAllStates(eventName, object, "paused");
-    }
-
-    resume(eventName, object) {
-        this.setAllStates(eventName, object, "listening");
-    }
-
-    removeAll() {
-        for (let pair of this.connections) {
-            const [rawObj, eventData] = pair;
-            eventData.forEach(listener => listener.disconnect());
-            this.connections.delete(rawObj);
-        }
-    }
-
-    // create utility functions for this later
-    remove(eventName, object) {
-        let rawObj;
-        let eventData;
-
-        if (object) {
-            rawObj = getRawObject(object);
-            eventData = this.connections.get(rawObj); 
-            const selectAll = eventName === "all";
-
-            for (let i in eventData) {
-                const listener = eventData[i];
-                if (listener.name === eventName || selectAll) {
-                    listener.disconnect();
-                    eventData.splice(i, 1);
-                }
+        /* 
+            this.connections [ListenerObject] = {
+                "eventName" [ListenerObject]: [
+                    ListenerObject,
+                    ListenerObject,
+                    ListenerObject,
+                ]
             }
-        } else {
-            const listener = eventName;
-            rawObj = getRawObject(listener.obj);
-            eventData = this.connections.get(rawObj);
-
-            for (let i in eventData) {
-                if (listener === eventData[i]) {
-                    listener.disconnect();
-                    eventData.splice(i, 1);
-                }
-            }
-        }
-
-        if (eventData.length === 0) {
-            this.connections.delete(rawObj);
-        }
+        */
+        this.connections = {}; 
     }
 
-    add(eventName, object, func) {
-        const rawObj = getRawObject(object);
-        let eventData = this.connections.get(rawObj);
-
-        // event data for a given object
-        if (!eventData) {
-            eventData = [];
-            this.connections.set(rawObj, eventData);
-        }
-
-        // new individual event listener data
-        const localListener = new Listener(
-            eventName,
-            object,
-            func,
-            funcHandler
-        )
-
-        // add listener data to event data
-        eventData.push(localListener);
-    
-        const that = this; // EventHandler object
-        function funcHandler(jsEvent) {
-            // console.log("global: ", that.state, " local: ", localListener.state);
-            if (that.isState("listening") && localListener.isState("listening")) {
-                func(jsEvent);
-            }
-        }
-    
-        // add js listener
-        object[eventConnectorName](eventName, funcHandler);
-        return localListener;
-    }
+    add(
 }
+
+
