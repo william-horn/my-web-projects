6e67bbd3da417d30029b6ed9c8eecab4cbda39af
William Horn <williamjosephhorn@gmail.com>
2022-03-25 09:47:43 -0400

    more weather api progress

--- developing/weather-api/resources/workspace/js/main.js
+++ developing/weather-api/resources/workspace/js/main.js
@@ -17,7 +17,15 @@
 | ABOUT DOCUMENT |
 ==================================================================================================================================
 
-Handle Weather Dashboard logic
+This program handles the main Weather Dashboard logic.
+
+* Technologies used:
+    - jQuery
+    - openweathermap API
+    - Google Fonts API
+
+* openweatherapp API key:
+    - 594655f7cc53f85edac45ab1fd9d4a8a
 
 ==================================================================================================================================
 
@@ -42,21 +50,6 @@ Handle Weather Dashboard logic
 import datastore from "../../../../../tools/api/general/js/datastore-1.0.0.js";
 import PseudoEvent from "../../../../../tools/api/general/js/pseudo-events-2.1.0.js";
 
-// working link: https://api.openweathermap.org/data/2.5/weather?q=Raleigh&APPID=19eecb01033710945577be8f1d9f7976
-// my api key: 594655f7cc53f85edac45ab1fd9d4a8a
-
-async function getRequest(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-}
-
-const data = getRequest("https://api.openweathermap.org/data/2.5/weather?q=Raleigh,NC,US&appid=594655f7cc53f85edac45ab1fd9d4a8a");
-data.then(data => {
-    console.log("finally: ", data);
-})
-
-
 /* ------------------------- */
 /* Global Element References */
 /* ------------------------- */
@@ -64,20 +57,41 @@ data.then(data => {
 /* ----------------------- */
 /* Internal Program States */
 /* ----------------------- */
+const weatherAPIKey = "594655f7cc53f85edac45ab1fd9d4a8a";
 
 /* ---------------------- */
 /* General Util Functions */
 /* ---------------------- */
+async function getAPIRequest(url, key) {
+    const response = await fetch(url + "&appid=" + key);
+    const data = await response.json();
+    return data;
+}
 
 /* ------------------------ */
 /* Dedicated Util Functions */
 /* ------------------------ */
+function generateWeatherCard(weatherData) {
+    
+}
 
 /* ------------------------ */
 /* Event Callback Functions */
 /* ------------------------ */
+function init() {
+    
+    const data = getAPIRequest(
+        "https://api.openweathermap.org/data/2.5/weather?q=Raleigh,NC,US",
+        weatherAPIKey
+    );
 
+    data.then(data => {
+        console.log("finally: ", data);
+    })
+
+}
 
 /* -------------------------- */
 /* Connect Js Event Listeners */
 /* -------------------------- */
+$(() => init()) // init program when document is ready
